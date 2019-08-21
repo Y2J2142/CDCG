f52635c18af3cf5fa15d88f711d14e3e7a32039d
Y2J2142 <Y2J2142@hotmail.com>
2019-08-21 23:45:21 +0200

    SBOVector example updated

--- examples/SBOVector.cpp
+++ examples/SBOVector.cpp
@@ -1,40 +1,51 @@
 #include "SBOVector.hpp"
 #include <iostream>
-
-//Non trivialy copyable type to test behaviour for both cases handled in the SBOVector
-struct B {
-	int t = 10;
-	B() = default;
-	B(B const&) { this->t += 1; }
-	B& operator=(B const&) {
-		++t;
-		return *this;
-	}
-};
-
+#include <string>
 
 
 
 int main () {
-    CDCG::Containers::SBOVector<int> v{};
-    CDCG::Containers::SBOVector<B> a{};
+    CDCG::Containers::SBOVector<int> ints{};
+    CDCG::Containers::SBOVector<std::string> strings{};
 
-    v.reserve(20);
+    ints.reserve(20);
     
 
 	for (int i = 0; i < 10; ++i) {
-        v.push_back(i);
-		a.push_back(B{});
+        ints.push_back(i);
 	}
     
-    for(auto && i : v)
+    for(auto && i : ints)
         std::cout << i << '\n';
 
-    a.emplace_back();
+    strings.emplace_back("test");
+    strings.emplace_back("test1");
+    strings.emplace_back("test2");
+    strings.emplace_back("test3");
+    strings.emplace_back("test4");
+    strings.emplace_back("test5");
+    strings.emplace_back("test6");
+    strings.emplace_back("test7");
 
-    for(auto && i : a)
-        std::cout << i.t << '\n';
 
+    auto movedStrings(std::move(strings));
+
+    CDCG::Containers::SBOVector<std::string> moveAssigned{};
+
+    moveAssigned.emplace_back("test");
+    moveAssigned.emplace_back("test11");
+    moveAssigned.emplace_back("test12");
+    moveAssigned.emplace_back("test13");
+    moveAssigned.emplace_back("test14");
+    moveAssigned.emplace_back("test15");
+    moveAssigned.emplace_back("test16");
+    moveAssigned.emplace_back("test17");
+    
+
+    moveAssigned = std::move(movedStrings);
+
+    for(auto&& s : moveAssigned)
+        std::cout << s << '\n';
 
 
     return 0;
