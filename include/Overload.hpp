84821cd6c8fd5f7410029a8a8d39d960de309c7b
Y2J2142 <Y2J2142@hotmail.com>
2020-01-12 01:00:52 +0100

    Overload  struct added

---
+++ include/Overload.hpp
@@ -0,0 +1,14 @@
+
+namespace CDCG {
+
+
+template<typename ...Base>
+struct Overload : Base... {
+    using Base::operator()...;
+};
+
+template<typename ...Args>
+Overload(Args&&... args) -> Overload<Args...>;
+
+
+}
---
+++ tests/Overloadtest.cpp
@@ -0,0 +1,55 @@
+#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_COLOUR_WINDOWS
+#include <catch2/catch.hpp>
+
+
+#include "Overload.hpp"
+#include <array>
+#include <variant>
+#include <type_traits>
+
+
+TEST_CASE("Overload usage") {
+
+    using Variant = std::variant<int, double, char>;
+
+
+    auto overload = CDCG::Overload{
+        [](int){ return 1; },
+        [](double){ return 2; },
+        [](char){ return 3; }
+    };
+
+    std::array<Variant,3> ar = {1, 2.0, '3'};
+    
+
+
+    REQUIRE( std::visit(overload, ar[0]) == 1);
+    REQUIRE( std::visit(overload, ar[1]) == 2);
+    REQUIRE( std::visit(overload, ar[2]) == 3);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+    int result = Catch::Session().run(argc, argv);
+
+    return result;
+}
