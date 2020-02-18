4830121783cfe5fc1d98d193bd1f18b3e2708ee6
Y2J2142 <Y2J2142@hotmail.com>
2020-02-18 19:34:25 +0100

    clang format applied to tests

--- tests/LazyArray-test.cpp
+++ tests/LazyArray-test.cpp
@@ -1,78 +1,77 @@
-#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER // This tells Catch to provide a main() - only do
+							// this in one cpp file
 #include <catch2/catch.hpp>
 
 #include "LazyArray.hpp"
 
-TEST_CASE("Default size and capacity") {
-
-    CDCG::LazyArray<int, 10> ar {};
-
-
-    REQUIRE( ar.size() == 0);
-    REQUIRE( ar.capacity() == 10);
-    REQUIRE( ar.empty() );
-
-}
-
-TEST_CASE( "Appending values") {
-    CDCG::LazyArray<int, 10> ar {};
-
-    ar.push_back(1);
-    ar.push_back(1);
-    ar.push_back(1);
-
-    REQUIRE( ar.size() == 3);
-    REQUIRE( !ar.empty() );
-
-}
-
-TEST_CASE( "Getting values from the array") {
-    CDCG::LazyArray<int, 10> ar {};
-
-    ar.push_back(1);
-    ar.push_back(1);
-    ar.push_back(1);
-    REQUIRE( ar.size() == 3);
-    REQUIRE( ar[0] == 1 );
-    REQUIRE( ar[1] == 1 );
-    REQUIRE( ar.at(2) == 1 );
-}
-
-TEST_CASE( "Clearing the array") {
-    CDCG::LazyArray<int, 10> ar {};
-    
-    ar.push_back(1);
-    ar.push_back(1);
-    ar.push_back(1);
-
-
-    ar.clear();
-    REQUIRE( ar.size() == 0);
-    REQUIRE( ar.empty() );
-
-
-}
-
-TEST_CASE("Equality Compare") {
-    CDCG::LazyArray<int, 15> ar1 = {1,2,3,4,5};
-    CDCG::LazyArray<int, 5> ar2{};
-    ar2.emplace_back(1);
-    ar2.emplace_back(2);
-    ar2.emplace_back(3);
-    ar2.emplace_back(4);
-    ar2.emplace_back(5);
-
-    REQUIRE(ar1 == ar2);
-    ar1.emplace_back(3);
-    REQUIRE(ar1 != ar2);
-
-}
-
-
-int main(int argc, char* argv[])
+TEST_CASE("Default size and capacity")
 {
 
-    int result = Catch::Session().run(argc, argv);
+	CDCG::LazyArray<int, 10> ar{};
 
-    return result;
+	REQUIRE(ar.size() == 0);
+	REQUIRE(ar.capacity() == 10);
+	REQUIRE(ar.empty());
+}
+
+TEST_CASE("Appending values")
+{
+	CDCG::LazyArray<int, 10> ar{};
+
+	ar.push_back(1);
+	ar.push_back(1);
+	ar.push_back(1);
+
+	REQUIRE(ar.size() == 3);
+	REQUIRE(!ar.empty());
+}
+
+TEST_CASE("Getting values from the array")
+{
+	CDCG::LazyArray<int, 10> ar{};
+
+	ar.push_back(1);
+	ar.push_back(1);
+	ar.push_back(1);
+	REQUIRE(ar.size() == 3);
+	REQUIRE(ar[0] == 1);
+	REQUIRE(ar[1] == 1);
+	REQUIRE(ar.at(2) == 1);
+}
+
+TEST_CASE("Clearing the array")
+{
+	CDCG::LazyArray<int, 10> ar{};
+
+	ar.push_back(1);
+	ar.push_back(1);
+	ar.push_back(1);
+
+	ar.clear();
+	REQUIRE(ar.size() == 0);
+	REQUIRE(ar.empty());
+}
+
+TEST_CASE("Equality Compare")
+{
+	CDCG::LazyArray<int, 15> ar1 = { 1, 2, 3, 4, 5 };
+	CDCG::LazyArray<int, 5> ar2{};
+	ar2.emplace_back(1);
+	ar2.emplace_back(2);
+	ar2.emplace_back(3);
+	ar2.emplace_back(4);
+	ar2.emplace_back(5);
+
+	REQUIRE(ar1 == ar2);
+	ar1.emplace_back(3);
+	REQUIRE(ar1 != ar2);
+}
+
+int
+main(int argc, char* argv[])
+{
+
+	int result = Catch::Session().run(argc, argv);
+
+	return result;
 }
--- tests/Range-test.cpp
+++ tests/Range-test.cpp
@@ -1,124 +1,123 @@
-#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER // This tells Catch to provide a main() - only do
+							// this in one cpp file
 #include <catch2/catch.hpp>
-
 
 #include "Range.hpp"
 #include <vector>
 
 using CDCG::Range;
 
-TEST_CASE("Range size calculation for integral ranges") {
-
-
-
-    SECTION("Only end of range specified") {
-        Range r{10};
-        REQUIRE( r.size() == 10 );
-    }
-    SECTION("End and begin of range specified") {
-        Range r2{2, 10};
-        REQUIRE( r2.size() == 8 );
-    }
-    SECTION("End, begin and step of range specified") {
-        Range r3{1, 10, 2};
-        REQUIRE( r3.size() == 4 );
-    }
-
-
-
-
-}
-
-TEST_CASE("Range size calculation for floating point ranges") {
-
-
-
-    SECTION("Only end of range specified") {
-        Range r{10.f};
-        REQUIRE( r.size() == 10 );
-    }
-    SECTION("End and begin of range specified") {
-        Range r2{2.f, 10.f};
-        REQUIRE( r2.size() == 8 );
-    }
-    SECTION("End, begin and step of range specified") {
-        Range r3{0.f, 10.f, 0.1f};
-        REQUIRE( r3.size() == 100 );
-    }
-
-}
-
-
-TEST_CASE("Iteration over integral range") {
-
-
-
-    SECTION("Only end of range specified") {
-        std::vector<int> vec{}; 
-        for(auto i : Range{10})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{0,1,2,3,4,5,6,7,8,9} == vec );
-    }
-
-    SECTION("End and begin of range specified") {
-        std::vector<int> vec{}; 
-        for(auto i : Range{1, 10})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{1,2,3,4,5,6,7,8,9} == vec );
-    }
-
-    SECTION("End, begin and step of range specified") {
-        std::vector<int> vec{}; 
-        for(auto i : Range{1, 10, 2})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{1,3,5,7,9} == vec );
-    }
-
-
-}
-
-//Testing for equality with floating point aint the best idea but its honest work
-TEST_CASE("Iteration over floating point range") {
-
-
-
-    SECTION("Only end of range specified") {
-        std::vector<float> vec{}; 
-        for(auto i : Range{10.f})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{0.f,1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f} == vec );
-    }
-
-    SECTION("End and begin of range specified") {
-        std::vector<float> vec{}; 
-        for(auto i : Range{1.f, 10.f})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f} == vec );
-    }
-
-    SECTION("End, begin and step of range specified") {
-        std::vector<float> vec{}; 
-        for(auto i : Range{0.f, 10.f, 2.f})
-            vec.push_back(i); 
-
-        REQUIRE( std::vector{0.f,2.f,4.f,6.f,8.f} == vec );
-    }
-
-
-}
-
-
-
-
-int main(int argc, char* argv[])
+TEST_CASE("Range size calculation for integral ranges")
 {
 
-    int result = Catch::Session().run(argc, argv);
+	SECTION("Only end of range specified")
+	{
+		Range r{ 10 };
+		REQUIRE(r.size() == 10);
+	}
+	SECTION("End and begin of range specified")
+	{
+		Range r2{ 2, 10 };
+		REQUIRE(r2.size() == 8);
+	}
+	SECTION("End, begin and step of range specified")
+	{
+		Range r3{ 1, 10, 2 };
+		REQUIRE(r3.size() == 4);
+	}
+}
 
-    return result;
+TEST_CASE("Range size calculation for floating point ranges")
+{
+
+	SECTION("Only end of range specified")
+	{
+		Range r{ 10.f };
+		REQUIRE(r.size() == 10);
+	}
+	SECTION("End and begin of range specified")
+	{
+		Range r2{ 2.f, 10.f };
+		REQUIRE(r2.size() == 8);
+	}
+	SECTION("End, begin and step of range specified")
+	{
+		Range r3{ 0.f, 10.f, 0.1f };
+		REQUIRE(r3.size() == 100);
+	}
+}
+
+TEST_CASE("Iteration over integral range")
+{
+
+	SECTION("Only end of range specified")
+	{
+		std::vector<int> vec{};
+		for (auto i : Range{ 10 })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } == vec);
+	}
+
+	SECTION("End and begin of range specified")
+	{
+		std::vector<int> vec{};
+		for (auto i : Range{ 1, 10 })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{ 1, 2, 3, 4, 5, 6, 7, 8, 9 } == vec);
+	}
+
+	SECTION("End, begin and step of range specified")
+	{
+		std::vector<int> vec{};
+		for (auto i : Range{ 1, 10, 2 })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{ 1, 3, 5, 7, 9 } == vec);
+	}
+}
+
+// Testing for equality with floating point aint the best idea but its honest
+// work
+TEST_CASE("Iteration over floating point range")
+{
+
+	SECTION("Only end of range specified")
+	{
+		std::vector<float> vec{};
+		for (auto i : Range{ 10.f })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{
+					0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f } == vec);
+	}
+
+	SECTION("End and begin of range specified")
+	{
+		std::vector<float> vec{};
+		for (auto i : Range{ 1.f, 10.f })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{ 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f } ==
+				vec);
+	}
+
+	SECTION("End, begin and step of range specified")
+	{
+		std::vector<float> vec{};
+		for (auto i : Range{ 0.f, 10.f, 2.f })
+			vec.push_back(i);
+
+		REQUIRE(std::vector{ 0.f, 2.f, 4.f, 6.f, 8.f } == vec);
+	}
+}
+
+int
+main(int argc, char* argv[])
+{
+
+	int result = Catch::Session().run(argc, argv);
+
+	return result;
 }
